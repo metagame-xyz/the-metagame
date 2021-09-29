58231ca1046c0135160a9ac42fe9746f6f76b693
Brenner Spear <blspear@gmail.com>
2021-09-29 00:15:55 -0500

    first test commit

---
+++ pages/api/newTransaction.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  console.log('query', req.query);
+  console.log('body', req.body)
+
+  res.status(200);
+}

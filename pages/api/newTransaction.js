876e37e12f491dca529b1286d74698b1826d5f04
Brenner Spear <blspear@gmail.com>
2021-09-29 00:19:50 -0500

    fix...

--- pages/api/newTransaction.js
+++ pages/api/newTransaction.js
@@ -4,5 +4,5 @@ export default function handler(req, res) {
   console.log('query', req.query);
   console.log('body', req.body);
 
-  res.status(200).json(body);
+  res.status(200).json(req.body);
 }

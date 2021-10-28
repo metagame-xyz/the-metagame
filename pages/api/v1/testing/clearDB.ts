020ed4265796d76ca5908eaca913c30307c9ff9c
Brenner Spear <blspear@gmail.com>
2021-10-28 00:24:08 -0500

    add back clearing db

---
+++ pages/api/v1/testing/clearDB.ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { ioredisClient } from '../../../../utils/utils';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // const [cursor, keys] = await ioredisClient.keys
+
+    // console.log('cursor:', cursor);
+    // console.log('keys:', keys);
+
+    // Create a readable stream (object mode)
+    const stream = ioredisClient.scanStream();
+    stream.on('data', async (resultKeys) => {
+        // `resultKeys` is an array of strings representing key names.
+        // Note that resultKeys may contain 0 keys, and that it will sometimes
+        // contain duplicates due to SCAN's implementation in Redis.
+        const pipeline = ioredisClient.pipeline();
+
+        for (let i = 0; i < resultKeys.length; i++) {
+            pipeline.del(resultKeys[i]);
+            console.log('deleting', resultKeys[i]);
+        }
+
+        const data = await pipeline.exec();
+        console.log('data:', data);
+    });
+    stream.on('end', () => {
+        console.log('all keys have been visited');
+    });
+    res.status(200).send({ message: 'ok' });
+}
+
+// await ioredisClient.hset(newUserAddress, { tokenId, metadata: JSON.stringify(metadata) });
+
+// // index by tokenId
+// await ioredisClient.hset(tokenId, {
+//     address: newUserAddress,
+//     metadata: JSON.stringify(metadata),
+// });

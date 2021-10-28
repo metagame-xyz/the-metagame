8e3ecc953a6d12648af011f59984462c8ba60294
Julio <julioocz@gmail.com>
2021-10-27 20:46:37 -0500

    UI without background

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Box, Button, Container, Heading, HStack } from '@chakra-ui/react';
-import Image from 'next/image';
+import { Box, Button, Container, HStack, Text } from '@chakra-ui/react';
 import discordIcon from '../images/icons/discord.png';
 import openSeaIcon from '../images/icons/open-sea.png';
 import twitterIcon from '../images/icons/twitter.png';
 
-function Navbar(props) {
+function Navbar() {
     return (
         <Box as="header" paddingTop="40px">
             <Container maxW={1280}>
                 <Box d="flex" width="100%" justifyContent="space-between">
-                    <Heading as="h1" fontSize="50px" fontWeight="light">
+                    <Text as="h1" fontSize="50px" fontWeight="light">
                         Birthblock
-                    </Heading>
-                    <HStack align="center" spacing="41px">
+                    </Text>
+                    <HStack align="center" spacing="20px">
                         <img height="51px" width="51px" src={discordIcon.src} />
                         <img height="60px" width="60px" src={twitterIcon.src} />
                         <img height="55px" width="54px" src={openSeaIcon.src} />
@@ -23,8 +22,7 @@ function Navbar(props) {
                             width={209}
                             height={59}
                             fontSize={28}
-                            borderRadius={30}
-                            isFullWidth>
+                            borderRadius={30}>
                             Connect
                         </Button>
                     </HStack>
